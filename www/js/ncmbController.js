fc231ce97da9e3cdc728164ae9b9800c6a7fe8a4
k17rs016 <k17rs016@st.kyusan-u.ac.jp>
2019-12-02 14:29:06 +0900

    ユーザ登録処理の修正

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -9,25 +9,50 @@ var ncmbController = {
     currentUser: null,  // ログインしたユーザーのオブジェクトを格納
     screenSize: null,    // 画面サイズを格納
 
-  
-uuid: function() {
-var uuid = "", i, random;
-for (i = 0; i < 32; i++) {
-random = Math.random() * 16 | 0;
-if (i == 8 || i == 12 || i == 16 || i == 20) {
-uuid += "-"
-}
-uuid += (i == 12 ? 4 :
-(i == 16 ? (random & 3 | 8) :
-random)).toString(16);
-}
-return uuid;
-},
+
+    // スコア送信
+    sendScore: function(score) {
+      var self = this;
+      // [1]Score（クラス）を生成
+      var Score = self.ncmb.DataStore("ScoreClass");
+      // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+      var scoreData = new Score({score: score});
+      // [3]送信処理
+      scoreData.save().then(function (saved) {
+      //  alert("スコア送信完了！");
+      // ncmbController.js -> ncmbController -> sendScore()
+
+// 順位を求める
+// ”score” フィールドの値が score より大きいものを取得
+  Score.greaterThan("score", score)
+  .count()    // 件数を結果に含める
+  .fetchAll()
+  .then(function(scores){
+    // countの結果は、取得データscoresのcountプロパティに含まれる
+    // 0件のとき正しく動作するように条件分岐
+     var rank = (scores.count !== undefined) ? parseInt(scores.count) + 1 : 1;
+     // ダイアログの表示
+      if(typeof navigator.notification !== 'undefined'){
+        navigator.notification.alert(
+           "今回の順位は #" + rank + " でした！",
+           function(){},
+            "スコア送信完了！"
+             );
+              } else {
+                alert("スコア送信完了！\n今回の順位は #" + rank + " でした！");
+        }
+    })
+        })
+        .catch(function(err){
+          console.log(err);
+          });
+          },
+
 
 // ncmbController.js -> ncmbController
 
 // ユーザー登録
-createUser: function() {
+ createUser: function() {
     var self = this;
 
     //適当なUUIDを作成
@@ -48,50 +73,20 @@ createUser: function() {
             // userName が被った場合はエラーが返る
             alert("ユーザー登録に失敗しました");
         });
-       
-},
+ },
 
-
-
-    // スコア送信
-    sendScore: function(score) {
-      var self = this;
-      // [1]Score（クラス）を生成
-      var Score = self.ncmb.DataStore("ScoreClass");
-      // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
-      var scoreData = new Score({score: score});
-      // [3]送信処理
-      scoreData.save().then(function (saved) {
-      //  alert("スコア送信完了！");
-      // ncmbController.js -> ncmbController -> sendScore()
-
-// 順位を求める
-// ”score” フィールドの値が score より大きいものを取得
-Score.greaterThan("score", score)
-    .count()    // 件数を結果に含める
-    .fetchAll()
-    .then(function(scores){
-        // countの結果は、取得データscoresのcountプロパティに含まれる
-
-        // 0件のとき正しく動作するように条件分岐
-        var rank = (scores.count !== undefined) ? parseInt(scores.count) + 1 : 1;
-
-        // ダイアログの表示
-        if(typeof navigator.notification !== 'undefined'){
-            navigator.notification.alert(
-                "今回の順位は #" + rank + " でした！",
-                function(){},
-                "スコア送信完了！"
-                );
-        } else {
-            alert("スコア送信完了！\n今回の順位は #" + rank + " でした！");
-        }
-    })
-        })
-        .catch(function(err){
-          console.log(err);
-          });
-          },
+ uuid: function() {
+   var uuid = "", i, random;
+   for (i = 0; i < 32; i++) {
+     random = Math.random() * 16 | 0;
+     if (i == 8 || i == 12 || i == 16 || i == 20) {
+       uuid += "-"
+       }
+       uuid += (i == 12 ? 4 :
+       (i == 16 ? (random & 3 | 8) :random)).toString(16);
+       }
+       return uuid;
+    },
 
     // 初期化
     init: function(screenSize) {
